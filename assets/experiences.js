87721240d1ed5b131c26878ee6825beef573f387
noizbuster <noizbuster@noizbuster.com>
2019-01-12 21:05:28 +0900

    implements draft of portfolio

--- assets/experiences.js
+++ assets/experiences.js
@@ -1,28 +1,88 @@
-module.exports = {
+export default {
   items: [
     {
-      title: '3f01932jf',
+      title: 'YujinRobot',
+      subtitle: 'Yeonsu-gu, Incheon, Korea',
+      position: 'Software Developer',
+      discription: 'Software Architecting & System Design\n' +
+        '* Micro Service Architecture\n' +
+        '* RESTful API\n' +
+        'Back-End\n' +
+        '* API Gateway\n' +
+        '　* Node.js (Actionhero)\n' +
+        '* Distributed Job Scheduler\n' +
+        '　* Node.js (Express.js)\n' +
+        '　* MongoDB\n' +
+        '* Fleet Management System\n' +
+        '　* Node.js (Express.js)\n' +
+        '　* Distributed Task Scheduler\n' +
+        '* Resource Managing & Config Server\n' +
+        '　* Node.js (Express.js)\n' +
+        '　* MongoDB, Sqlite\n' +
+        '* Database\n' +
+        '　* MongoDB (RS, Transaction)\n' +
+        '　* Sqlite\n' +
+        '　* MariaDB\n' +
+        'Front-End\n' +
+        '* IoT Service Platform Operation & Monitoring Tool (A.K.A. Balcony)\n' +
+        '　* AngularJS 1.x\n' +
+        '* Robot Monitoring & Environment(resource) Manager\n' +
+        '　* Angular.io (version 2^)\n' +
+        'DevOps\n' +
+        '* Deployment Pipeline\n' +
+        '　* docker, docker-compose\n' +
+        '* Server Deployment\n' +
+        '　* Ansible\n' +
+        '* Logging (ELK)\n' +
+        '　* Elastic Search\n' +
+        '　* LogStash + Filebeat\n' +
+        '　* Kibana See less',
       color: 'primary',
-      icon: 'mdi-star',
-      year: 'ewf'
+      icon: 'domain',
+      year: '2016 - NOW',
+      period: 'january 18, 2016 ~ Present',
+      projects: []
     },
     {
-      title: '3f01932jf',
+      title: 'Frankly Chat',
+      subtitle: 'Bay Area, San Francisco, United States',
+      position: '(Intern) Software Engineer',
+      discription: 'Software Develop\n' +
+        '* Android Client\n' +
+        '* iOS Client\n' +
+        '* Keyword Analysis System\n' +
+        '* Machine Learning (mahout, solr lucene)',
       color: 'accent',
-      icon: 'mdi-book-variant',
-      year: 'ewf'
+      icon: 'domain',
+      year: '2014 - 2015',
+      period: 'september 15, 2014 ~ march 16, 2015 (6 months)',
+      projects: []
     },
     {
-      title: '3f01932jf',
+      title: 'ITSP (IT Security & Privacy)',
+      subtitle: 'Lab of Kookmin University',
+      position: 'Researcher',
+      discription: '* Cryptography \n' +
+        '* Android Security \n' +
+        '* Mathematics',
+      color: 'secondary',
+      icon: 'school',
+      year: '2014 - 2015',
+      period: 'august 19, 2015 ~ february 24, 2016 (2 years 6 months)',
+      projects: []
+    },
+    {
+      title: 'VIEWORKS',
+      subtitle: 'Sungnam, Korea',
+      position: '(Intern) Software QA',
+      discription: 'Software Alpha test, Bug reporting \n' +
+        'Issu Tracking tool ‘Trac’ use experience acquired \n' +
+        'Working with foreign workers',
       color: 'success',
-      icon: 'mdi-airballoon',
-      year: 'ewf'
-    },
-    {
-      title: '3f01932jf',
-      color: 'failed',
-      icon: 'mdi-buffer',
-      year: 'ewf'
+      icon: 'domain',
+      year: '2011 - 2012',
+      period: 'december 1, 2011 ~ may 1, 2012 (6 months)',
+      projects: []
     }
   ]
-}
+};
--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,6 +1,6 @@
 <template>
   <v-layout pl-3>
-    <v-timeline dense align-top clipped>
+    <v-timeline style="width:100%" dense align-top clipped>
       <v-timeline-item
         v-for="(item, i) in items"
         :color="item.color"
@@ -13,9 +13,25 @@
           :color="item.color"
           dark
         >
-          <v-card-title class="title">{{ item.title }}</v-card-title>
+          <!--head-->
+          <v-card-title>
+            <v-layout column>
+              <flex class="title">
+                <v-layout justify-space-between>
+                  <v-flex>{{ item.title }}</v-flex>
+                  <v-flex text-xs-right class="subheading">
+                    {{ item.year }}
+                  </v-flex>
+                </v-layout>
+              </flex>
+              <flex class="subheading">{{ item.subtitle }}</flex>
+            </v-layout>
+          </v-card-title>
+          <!--body-->
           <v-card-text class="white text--primary">
-            <p>Lorem ipsum dolor sit amet, sententiae.</p>
+            <p class="subheading ma-0 font-weight-bold">{{ item.position }}</p>
+            <p class="caption">{{ item.period }}</p>
+            <pre> {{ item.discription }}</pre>
           </v-card-text>
         </v-card>
       </v-timeline-item>
