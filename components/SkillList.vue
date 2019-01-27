4f2932a64ff7dd3428b78e3e1f8836c7e583957d
NoizBuster <noizbuster@noizbuster.com>
2019-01-28 01:52:49 +0900

    add related project on skill list

--- components/SkillList.vue
+++ components/SkillList.vue
@@ -1,10 +1,15 @@
 <template>
-  <v-expansion-panel>
+  <v-expansion-panel focusable>
     <!--TODO add button to open close all body-->
     <v-expansion-panel-content
       v-for="(item, idx) in skills"
-      :key="idx"
+      :key="'skills'+idx"
+      :readonly="item.projects.length === 0"
     >
+      <v-icon v-if="item.projects.length === 0"
+              slot="actions"
+              color="primary">remove
+      </v-icon>
       <!--head-->
       <div slot="header">
         <v-layout wrap>
@@ -32,12 +37,19 @@
         </v-layout>
       </div>
       <!--body-->
-      <v-card>
-        <v-card-text>
-          <!--TODO related projects-->
-          <!--TODO description in markdown-->
-        </v-card-text>
-      </v-card>
+      <v-divider></v-divider>
+      <v-list style="background: none">
+        <v-subheader class="headline">
+          Relative Project{{ item.projects.length > 1 ? 's' : '' }}
+        </v-subheader>
+        <v-list-tile v-for="(p, idx) in item.projects"
+                     :key="item.name+'relative'+idx">
+          <v-list-tile-content>
+            <v-list-tile-title>{{ p.name }}</v-list-tile-title>
+            <v-list-tile-sub-title>{{ p.hint }}</v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-expansion-panel-content>
 
   </v-expansion-panel>
@@ -46,16 +58,39 @@
 <script>
   import TagBadge from '~/components/TagBadge.vue';
   import SkillBadge from '~/components/SkillBadge.vue';
+  import catagorizedProjects from '~/assets/projects.js';
 
   export default {
     components: {TagBadge, SkillBadge},
     props: {skills: {type: Object, default: undefined}},
     data() {
       let prefix = '/portfolio';
-      if(process.env.DEPLOY_ENV !== 'GH_PAGES'){
+      if (process.env.DEPLOY_ENV !== 'GH_PAGES') {
         prefix = '';
       }
       return {show: true, prefix: prefix}
+    },
+    created() {
+      const hasSkill = function (project, skillname) {
+        let tech = _.findIndex(project.technologies, (tech) => {
+          return tech.name.includes(skillname)
+        });
+        let base_tech = _.findIndex(project.base_technologies, (tech) => {
+          return tech.name.includes(skillname)
+        });
+        return tech !== -1 || base_tech !== -1;
+      };
+      _.map(this.skills, (skill, skillId) => {
+        let relatedProjects = [];
+        _.forEach(catagorizedProjects, (projects) => {
+          const related = _.filter(projects, (project) => {
+            console.log(project.name, 'has skill', skillId, ': ', hasSkill(project, skillId));
+            return hasSkill(project, skillId)
+          })
+          relatedProjects = _.concat(relatedProjects, related);
+        });
+        skill.projects = relatedProjects;
+      })
     }
   }
 </script>
